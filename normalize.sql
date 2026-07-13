7f9b8f1fb5832ef23475fdbca8dc0867f009b6aa
Dominik Zalewski <dominikz@drknowhow.co>
2026-07-13 17:27:56 +0200

    cli: move value parsing to normalize.sql

--- normalize.sql
+++ normalize.sql
@@ -1,2 +1,17 @@
 DROP TABLE IF EXISTS invoices;
-CREATE TABLE invoices AS SELECT * FROM raw_invoices;
+CREATE TABLE invoices AS
+SELECT
+  grouping,
+  product,
+  description,
+  reference,
+  CAST(quantity AS REAL) AS quantity,
+  from_date,
+  until_date,
+  condition,
+  unit,
+  external_id,
+  price,
+  CAST(TRIM(REPLACE(REPLACE(REPLACE(total, '€', ''), '$', ''), ',', '')) AS REAL) AS total,
+  CASE WHEN total LIKE '%$%' THEN 'usd' ELSE 'eur' END              AS currency
+FROM raw_invoices;
